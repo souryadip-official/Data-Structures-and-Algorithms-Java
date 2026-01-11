76989a3c4f31889deb8080ffe8772e0694623311
Souryadip Mallick <souryadipmallick118@gmail.com>
2026-01-11 12:25:21 +0530

    11-01-2026 12:25PM

--- SegmentTree01.java
+++ SegmentTree01.java
@@ -27,11 +27,38 @@ public class SegmentTree01 {
     public static int getSum(int[] nums, int[] segmentTree, int queryStart, int queryEnd) {
         return getSumUtil(segmentTree, queryStart, queryEnd, 0, nums.length-1, 0);
     }
+    public static void updateUtil(int[] segmentTree, int diff, int idx, int start, int end, int curr) {
+        if (start == idx && end == idx) {
+            /* Single or leaf node condition */
+            segmentTree[curr] = segmentTree[curr] + diff;
+            return;
+        }
+        if (start <= idx && idx <= end) {
+            /* This node is included in the current range */
+            segmentTree[curr] = segmentTree[curr] + diff;
+            int mid = start + (end - start)/2;
+            if (idx <= mid) /* This particular idx lies in the [start, mid] range */
+                updateUtil(segmentTree, diff, idx, start, mid, curr*2+1);
+            else /* This particular idx lies in the [mid+1, end] range */
+                updateUtil(segmentTree, diff, idx, mid+1, end, curr*2+2);
+        } else {
+            /* It is not included in the current range */
+            return;
+        }
+    }
+    public static void update(int[] arr, int[] segmentTree, int idx, int newValue) {
+        if (idx < 0 || idx >= arr.length) return;
+        int diff = newValue - arr[idx];
+        arr[idx] = newValue;
+        updateUtil(segmentTree, diff, idx, 0, arr.length-1, 0);
+    }
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
         int[] segmentTree = new int[4 * nums.length];
         buildSegmentTree(nums, segmentTree, 0, nums.length-1, 0);
         System.out.println(Arrays.toString(segmentTree));
         System.out.println(getSum(nums, segmentTree, 2, 5));
+        update(nums, segmentTree, 2, 2);
+        System.out.println(Arrays.toString(segmentTree));
     }
 }
